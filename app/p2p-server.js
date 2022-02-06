8b582cdf246549cf0ebc5a513efe1dae3b798ba6
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-02-06 18:57:45 -0300

    feat: adiciona conexao P2P

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -3,3 +3,21 @@ const Websocket = require('ws')
 const P2P_PORT = process.env.P2P_PORT || 5001
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : []
 
+class P2pServer {
+
+  constructor(blockchain) {
+    this.blockchain = blockchain
+    this.socket = []
+  }
+
+  listen() {
+    const server = new Websocket.Server({ port: P2P_PORT })
+    server.on('connection', socket => this.connectSocket(socket))
+  }
+  
+  connectSocket(socket) {
+    this.socket.push(socket)
+    console.log('Socket connected')
+  }
+}
+
