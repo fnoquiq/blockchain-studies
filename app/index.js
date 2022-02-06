30200cb50bb04d79ee940e5fcae4b96f564f644f
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-02-06 19:10:44 -0300

    feat: cria servidor ws

--- app/index.js
+++ app/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const res = require('express/lib/response')
 
 const Blockchain = require('../blockchain')
+const P2pServer = require('./p2p-server')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const app = express()
 const bc = new Blockchain()
+const p2pServer = new P2pServer(bc)
 
 app.use(express.json())
 
@@ -26,3 +28,5 @@ app.post('/mine', (req, res) => {
 app.listen(HTTP_PORT, () => {
   console.log(`Listening on port ${HTTP_PORT}`)
 })
+
+p2pServer.listen()
