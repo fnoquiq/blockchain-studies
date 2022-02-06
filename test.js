e42b7e0c6e446f1985a0d6ffe25ad540980935fd
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-02-05 22:23:53 -0300

    test: adiciona test de Block

--- test.js
+++
@@ -1,7 +0,0 @@
-const Block = require('./block.js')
-
-const block = new Block('7657', '1234567890B21314', 'A1234567890F1231312', '100')
-console.log(block.toString())
-
-const primeiroBloco = Block.mineBlock(Block.genesis(), '$500')
-console.log(primeiroBloco.toString())
