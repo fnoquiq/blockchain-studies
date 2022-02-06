606fc6dcb28f20cd103b1ea20e9fe153e983eac5
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-02-05 23:00:22 -0300

    test: adiciona testes de isValidChain

--- blockchain.test.js
+++ blockchain.test.js
@@ -3,10 +3,11 @@ const Block = require('./block')
 
 describe('Blockchain', () => {
 
-  let bc
+  let bc, bc2
 
   beforeEach(() => {
     bc = new Blockchain()
+    bc2 = new Blockchain()
   })
 
   it('starts with genesis block', () => {
@@ -18,5 +19,24 @@ describe('Blockchain', () => {
     bc.addBlock(data)
 
     expect(bc.chain[bc.chain.length -1].data).toEqual(data)
+  })
+
+  it('validate a valid chain', () => {
+    bc2.addBlock('500U$')
+    
+    expect(bc.isValidChain(bc2.chain)).toBe(true)
+  })
+
+  it ('invalidate a chain with a corrupt genesis block', () => {
+    bc2.chain[0].data = '0U$'
+
+    expect(bc.isValidChain(bc2.chain)).toBe(false)
+  })
+
+  it('invalidate a corrupt chain', () => {
+    bc2.addBlock('200U$')
+    bc2.chain[1].data = '0U$'
+
+    expect(bc.isValidChain(bc2.chain)).toBe(false)
   })
 })
